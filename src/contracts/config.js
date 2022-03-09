995dc8ae3b629dee25055036da9c2e83e63d422d
DESKTOP-3J65I2E\admin <topdirector2017@gmail.com>
2022-03-09 23:12:22 +0900

    add source

---
+++ src/contracts/config.js
@@ -0,0 +1,47 @@
+const config = {
+  AnchorEarnBSC: {
+    0: "0x0000000000000000000000000000000000000000",
+    56: "0x0EA7C4c6A230e091e930b0E6fD9D5c18a6699Cbc",
+    97: "0x80de92df50F270e49bb4E269502D00cAB753A55a",
+  },
+  StakingVault: {
+    0: "0x0000000000000000000000000000000000000000",
+    56: "0x8355179C4e5b37101514181Efb87BDDA11b6D23f",
+    97: "0xd6861aB260a8c2f060d3E541Df7696dDBC9AB682",
+  },
+  BlockExplorerURL: {
+    56: "https://bscscan.com",
+    97: "https://testnet.bscscan.com",
+  },
+  RpcURL: {
+    56: "https://bsc-dataseed1.defibit.io/",
+    97: "https://speedy-nodes-nyc.moralis.io/03eb35954a0b7ed092444a8e/bsc/testnet",
+  },
+  chainHexID: {
+    56: "0x38",
+    97: "0x61",
+  },
+  MAX_STAKE_AMOUNT_PER_USER_DIV_DECIMALS: 10000,
+  INFURA_ID: "88b3ca144c6648df843909df0371ee08",
+  chainID: 56,
+  Website: "https://www.anchorearnbsc.com/",
+  Community: "https://t.me/AnchorEarnBSC",
+  Documentation: "https://read-whitepaper.gitbook.io/anchor-earn-bsc/",
+};
+
+export const Units = {
+  0: "noether",
+  1: "wei",
+  3: "kwei",
+  6: "mwei",
+  9: "gwei",
+  12: "micro",
+  15: "finney",
+  18: "ether",
+  21: "kether",
+  24: "mether",
+  27: "gether",
+  30: "tether",
+};
+
+export default config;
